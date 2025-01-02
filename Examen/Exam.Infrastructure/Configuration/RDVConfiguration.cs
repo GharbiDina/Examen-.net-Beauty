c9105fd4ff94124e419e78d30506ff5e5d3ddb07
Dina <100855498+GharbiDina@users.noreply.github.com>
2025-01-02 21:42:07 +0100

    a relation entre Prestation, RDV et Client
    Configurer la relation entre Prestation, RDV et Client afin que les clés
     étrangères de cette relation soient mappées vers des colonnes nommées
     PrestationFK et ClientFK.

---
+++ Examen/Exam.Infrastructure/Configuration/RDVConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Infrastructure.Configuration
+{
+    public class RDVConfiguration : IEntityTypeConfiguration<RDV>
+    {
+        public void Configure(EntityTypeBuilder<RDV> builder)
+        {
+            builder
+.HasOne(r => r.Client)
+.WithMany(p => p.RDV)
+.HasForeignKey(r => r.ClientFK);
+
+            builder
+            .HasOne(r => r.Prestation)
+            .WithMany(f => f.RDV)
+           .HasForeignKey(r => r.PrestationFK);
+            
+
+        }
+    }
+}
